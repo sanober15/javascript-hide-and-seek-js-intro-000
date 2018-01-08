970bd3b9b6b32fece59a816abfc94cf26f6f6487
sanober riaz <sanober@sanober.me>
2018-01-08 22:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function nestedTarget(){
   return document.getElementById('nested').querySelectorAll('div.target')[0]
 
 }
+
+function increaseRankBy(n){
+  const x = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < x.length; i++) {
+    x[i] = parseInt(x[i].innerHTML, 10) + n;
+  }
+}
