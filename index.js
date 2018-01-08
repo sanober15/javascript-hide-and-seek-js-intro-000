f9e60d668e5b8b49ce1303011a4785cc9366f430
sanober riaz <sanober@sanober.me>
2018-01-08 20:10:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function getFirstSelector(selector){
   return document.querySelector(selector);
 }
+
+function nestedTarget(){
+  return document.getElementById(nested).querySelectorAll(.target);
+}
+
+function deepestChild(){
+  
+}
+
+function increaseRankBy(){
+  
+}
